beb672853e6ad4740e2a74bc16d4af33eea285af
sedabasaran <seda_basaran_@hotmail.com>
2026-05-09 12:54:34 +0300

    commit

---
+++ src/main/java/com/fleettracking/fleet_tracker/FleetTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fleettracking.fleet_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleettracking/fleet_tracker/FleetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleettracking.fleet_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
